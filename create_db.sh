2b8e6fdf87f9e1dae85f2559e6baee5dd1ac1856
Oracle Public Cloud User <opc@instance-20220413-0307.subnet04130305.vcn04130305.oraclevcn.com>
2023-01-16 22:06:00 +0000

    create db step two

--- create_db.sh
+++ create_db.sh
@@ -8,21 +8,23 @@ read -p "Enter DB name : " db_name
 
 if [[ $db_name =~ ^[a-zA-Z][a-zA-Z0-9_]+$ ]]
 then
-	if [[ -d $db_name ]]
+	if [[ -d $HOME/$db_name ]]
 	then
 		clear
 		red "$db_name is already exists please choice another name"
 	else
 		 green "$db_name is valid"
+		mkdir $HOME/$db_name
 		break
 	fi
 else
 	clear
-	if ! [[ $db_name =~ ^[a-zA-z] ]]
-	then
+	if ! [[ $db_name == ^[a-zA-z] ]]
+ 	then
 		red "DB name Can't start with a non char"
 	fi
-	if [[ $db_name =~ [[:space:]] ]]
+	#if [[ $db_name =~ [[:space:]] ]]
+	if [[ $db_name == *" "* ]] 
 	then 
 		red "DB name Can't Have any Spaces"
 	fi
