53c993b89355935505b5c64baee41b7eddffd4e4
Nader Mohammed <89906715+nader-mohammed-20x@users.noreply.github.com>
2023-01-18 23:33:41 +0200

    Update create_table.sh

--- create_table.sh
+++ create_table.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-. ../../functions.sh
-function getDataType() {
+. functions.sh
+function getDataType(){
 	datatypeMenu=$(whiptail --title "Data Type Menu " --fb --menu "select Data Type" 15 60 4 \
 		"1" "int"\
 		"2" "str"\
@@ -35,8 +35,8 @@ function getTableName(){
 				whiptail --title "Create Table Message" --msgbox "Table [$table_name] is already exists." 8 45
 			else # No File Exists
 				green "$table_name is valid"
-				touch ./db/$db_name/$table_name
-				whiptail --title "Create Table" --msgbox "Table [$table_name] successfully created." 8 45
+				#touch ./db/$db_name/$table_name
+				#whiptail --title "Create Table" --msgbox "Table [$table_name] successfully created." 8 45
 				break
 			fi
 		else # if Table Name is not formated well 
@@ -61,7 +61,7 @@ function getTableName(){
 
 # -----------------------------------------------------------------------------
 yellow "Creating New Table" 
-echo "create table $PWD"
+db_name="nader" #FOR TESTING
 if [[ -z $db_name ]] #Empty 
 then
 	whiptail --title "Create Table" --msgbox "Connect TO DB first" 8 45
@@ -75,11 +75,67 @@ then
 else # DB Found
 	getTableName
 	echo "db/$db_name/$table_name"
-	if [[ -z $table_name ]]
+	if [[ ! -z $table_name ]] # Table Created Successfully
 	then
-		echo "creating Table Canceled by User...s"
-		. connectToDataBase.sh
-	else
 		green $table_name
+		
+		colsNum=$(whiptail --title "Table Is Valid" --inputbox "Enter columns number" 8 45 3>&1 1>&2 2>&3)
+		echo $colsNum
+		if [ $colsNum -gt 0 ]
+		then	
+			touch ./db/$db_name/$table_name
+			whiptail --title "Create Table" --msgbox "Table [$table_name] successfully created." 8 45
+			
+			#----------------------------------------------------
+			i=1
+			datatype=""
+			sep=":"
+			isPrimary=""
+			primarykeyMenu="2" # No PK in Menu
+			tableInfo="$colName$sep$datatype$sep$isPrimary"
+
+			while [ $i -le $colsNum ]
+			do
+				isPrimary=""
+				colName=$(whiptail --title "column Name" --inputbox "Enter Column $i Name "  8 45 3>&1 1>&2 2>&3)
+
+			     	getDataType
+
+			  	if [[ $primarykeyMenu == "2" ]] # make sure there is one PK
+			  	then
+					  primarykeyMenu=$(whiptail --title "Primary Key Menu " --fb --menu "Is column primary key?" 15 60 4 \
+						"1" "yes" \
+						"2" "no" 3>&1 1>&2 2>&3)
+					  case $primarykeyMenu in
+						1)
+							 isPrimary="PK"
+							 ;;
+						2)
+							 isPrimary=""
+							 ;;
+					  esac
+			  	fi
+				if [[ $i -eq $colsNum ]]; then
+				    echo  $colName >> $table_name;
+				    echo  $colName$sep$datatype$sep$isPrimary >> "db/$db_name/$table_name";
+
+				else
+				      echo -n $colName$sep >> $table_name;
+				      echo  $colName$sep$datatype$sep$isPrimary$sep >> "db/$db_name/$table_name";
+				fi
+					((i++)) # increase counter
+						
+			done	
+			whiptail --title "Create table Message" --msgbox "You created $table_name sucessfully" 8 45
+		#fi
+
+			#-----------------------------------------------------
+		else
+			whiptail --title "Columns Error" --msgbox "can't create [$colsNum] colums it has to be more than 0." 8 45
+		fi
+		
+	else # user cancel creation
+		echo "creating Table Canceled by User."
+		. connectToDataBase.sh
 	fi
 fi
