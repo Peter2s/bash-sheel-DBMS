204f21c374672da6f22fbd068fa7d392c9dcfd1e
Nader Mohammed <89906715+nader-mohammed-20x@users.noreply.github.com>
2023-01-18 19:51:02 +0200

    add create_db.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 function showMenu() {
     selectedOpt=$(whiptail --title "Main Menu" --fb --menu "Choose an option" 15 60 4 \
-        "1" "Craete DataBase" \
+        "1" "Create DataBase" \
         "2" "List DataBases" \
         "3" "Connect To DataBase"\
        "4" "Drop DataBase" 3>&1 1>&2 2>&3)
     case $selectedOpt in
         1)
-            echo "Craete DataBase"
+            . create_db.sh
         ;;
         2)
             dataBaseNo=$(ls -d */ | cut -f1 -d"/" | wc -l)
