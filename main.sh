f94530644b5321d0b0ff363ba98d9dcaa2121ee8
Nader Mohammed <89906715+nader-mohammed-20x@users.noreply.github.com>
2023-01-18 19:57:48 +0200

    add create_db and fetch dbs correctly

--- main.sh
+++ main.sh
@@ -4,19 +4,25 @@ function showMenu() {
         "1" "Create DataBase" \
         "2" "List DataBases" \
         "3" "Connect To DataBase"\
-        "4" "Drop DataBase" 3>&1 1>&2 2>&3)
+       "4" "Drop DataBase" 3>&1 1>&2 2>&3)
     case $selectedOpt in
         1)
             . create_db.sh
         ;;
         2)
-           ./listDataBases.sh
-	    showMenu    
+            dataBaseNo=$(ls -d db/* | cut -f1 -d"/" | wc -l)
+	    if (("$dataBaseNo" == 0 ))
+	    then
+		dataBaseList='No DataBase exist'
+	   else
+		dataBaseList=$(ls -d db/* | cut -f2 -d'/')
+	    fi
+	    whiptail --title "List of DataBases" --msgbox "Number Of DataBases : $dataBaseNo \n$dataBaseList" 10 45
+	    showMenu
+                        
         ;;
         3)
-        ./connecToDB.sh  
-        ;;
-        4)
+            echo "connect To DataBase"
         ;;
     esac
 }
