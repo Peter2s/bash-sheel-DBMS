b0db8d99a1ff4ac2c401560f0805e7771e3a6860
Nader Mohammed <89906715+nader-mohammed-20x@users.noreply.github.com>
2023-01-24 23:04:27 +0200

    Fix bugs

--- connectToDataBase.sh
+++ connectToDataBase.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . functions.sh
 db_name=$(whiptail --title "Connect to DataBase" --inputbox "Enter your DataBase name " 8 45 3>&1 1>&2 2>&3)
-if [[ -d db/$db_name ]]
+if [[ -d db/$db_name ]] && ! [[ -z $db_name ]]
 then
 	. table.sh
 else
-	whiptail --title "Error Message" --msgbox "Connection failed to $db_name" 8 45
+	whiptail --title "Error Message" --msgbox "Connection failed to [$db_name]" 8 45
 	#red "connection failed"
     . main.sh	
         
