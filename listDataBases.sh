7ee535ce7b63e84cdb352d95f55316da5dd2ff0f
Nader Mohammed <89906715+nader-mohammed-20x@users.noreply.github.com>
2023-01-24 23:05:34 +0200

    Dynamic listing

--- listDataBases.sh
+++ listDataBases.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 dataBaseNo=$(ls -d db/* | cut -f1 -d"/" | wc -l)
-	    if (("$dataBaseNo" == 0 ))
-	    then
-		dataBaseList='No DataBase exist'
-	   else
-		dataBaseList=$(ls -d db/* | cut -f2 -d'/')
-	    fi
-	    whiptail --title "List of DataBases" --scrolltext --msgbox "Number Of DataBases : $dataBaseNo \n$dataBaseList" 30 45
+if (("$dataBaseNo" == 0 ))
+then
+	dataBaseList='No DataBase exist'
+else
+	dataBaseList=$(ls -d db/* | cut -f2 -d'/')
+fi
+#echo $dataBaseNo;
+if [ $dataBaseNo -le 5 ]
+then
+	menu_cols=12
+else
+	((menu_cols=$dataBaseNo+8))
+fi
+#echo $dataBaseNo;
+whiptail --title "List of DataBases" --scrolltext --msgbox "Number Of DataBases : $dataBaseNo \n$dataBaseList" $menu_cols 45
